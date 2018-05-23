193088b149534bf23c4205860db0825a3f12a287
Adailson Santos <adailson2@gmail.com>
2018-05-23 18:08:57 -0300

    Add raw git clone for webapp repository

--- clone.sh
+++ clone.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/PI2-Estufa/temperatureServer
 git clone https://github.com/PI2-Estufa/humidityServer
 git clone https://github.com/PI2-Estufa/phServer
+git clone https://github.com/PI2-Estufa/webApp
