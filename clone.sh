dc8d2f1305b1b4dfde763bf7d7295b5932c46afb
Adailson Santos <adailson2@gmail.com>
2018-05-23 18:21:48 -0300

    Add raw for simulators repository

--- clone.sh
+++ clone.sh
@@ -3,3 +3,4 @@ git clone https://github.com/PI2-Estufa/humidityServer
 git clone https://github.com/PI2-Estufa/phServer
 git clone https://github.com/PI2-Estufa/webApp
 git clone https://github.com/PI2-Estufa/webService
+git clone https://github.com/PI2-Estufa/simulators 
