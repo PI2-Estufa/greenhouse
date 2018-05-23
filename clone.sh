d1a56443e65ceecfcf2bfbe784a4d1f1d23f97ed
Adailson Santos <adailson2@gmail.com>
2018-05-23 18:19:30 -0300

    Add raw for api in clone.sh

--- clone.sh
+++ clone.sh
@@ -2,3 +2,4 @@ git clone https://github.com/PI2-Estufa/temperatureServer
 git clone https://github.com/PI2-Estufa/humidityServer
 git clone https://github.com/PI2-Estufa/phServer
 git clone https://github.com/PI2-Estufa/webApp
+git clone https://github.com/PI2-Estufa/webService
