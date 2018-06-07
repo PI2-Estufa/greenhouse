7e3f6d5b428ef93df25e4e4ba2c3f865b0e6fe76
Adailson Santos <adailson2@gmail.com>
2018-06-07 17:27:06 -0300

    Add water leval in clone sh

--- clone.sh
+++ clone.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/PI2-Estufa/temperatureServer
 git clone https://github.com/PI2-Estufa/humidityServer
 git clone https://github.com/PI2-Estufa/phServer
+git clone https://github.com/PI2-Estufa/waterLevelServer
 git clone https://github.com/PI2-Estufa/webApp
 git clone https://github.com/PI2-Estufa/webService
 git clone https://github.com/PI2-Estufa/simulators 
