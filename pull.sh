368fbe23b496d93f710e3d5f3a9cc71ca667d7e5
Adailson Santos <adailson2@gmail.com>
2018-06-07 16:43:47 -0300

    Automatize pull repositories

---
+++ pull.sh
@@ -0,0 +1,25 @@
+cd /temperatureServer/
+git checkout .
+cd ../humidityServer/ 
+git checkout .
+cd ../phServer/ 
+git checkout .
+cd ../webApp/
+git checkout .
+cd ../webService/
+git checkout .
+cd ../simulators
+git checkout .
+
+cd /temperatureServer/
+git pull origin master
+cd ../humidityServer/ 
+git pull origin master
+cd ../phServer/ 
+git pull origin master
+cd ../webApp/
+git pull origin master
+cd ../webService/
+git pull origin master
+cd ../simulators
+git pull origin master
